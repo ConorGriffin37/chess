a53e14e16eb89904c529b281782bd77fb0e36c53
Terry Bolt <bolt.terry@gmail.com>
2015-02-03 17:52:00 +0000

    Added a Piece class to represent a single piece. Also renamed the directory structure to conform to C# naming conventions.

---
+++ gui/GUI/Board.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace GUI
+{
+	public enum PieceColour { White, Black };
+	public enum PieceType { Pawn, Knight, Bishop, Rook, Queen, King };
+
+	/**
+	 * @brief Representation of the board as a whole.
+	 */
+	public class Board
+	{
+		public Board ()
+		{
+		}
+	}
+
+	/**
+	 * @brief Representation of a single piece.
+	 * 
+	 * Note that it contains no default constructor as there is no "base" piece.
+	 */
+	public class Piece
+	{
+		public PieceColour Colour { get; private set; }
+		public PieceType Type { get; private set; }
+
+		/**
+		 * @brief Constructor for a Piece.
+		 * 
+		 * @param colour	the colour of the piece.
+		 * @param type		the type of the piece.
+		 */
+		public Piece(PieceColour colour, PieceType type)
+		{
+			Colour = colour;
+			Type = type;
+		}
+
+		/**
+		 * @brief Copy constructor for a piece.
+		 * 
+		 * @param other		the piece from which to copy.
+		 */
+		public Piece(Piece other)
+		{
+			Colour = other.Colour;
+			Type = other.Type;
+		}
+
+		/**
+		 * @brief Tests whether a piece is equal to another.
+		 * 
+		 * Override of @c Object.Equals. First tests whether @c obj is null, then attempts to cast to
+		 * Piece. If it is still not null, it compares the @c PieceType and @c PieceColour properties.
+		 * 
+		 * @param obj	the object to compare to.
+		 */
+		public override bool Equals (object obj)
+		{
+			if (obj == null)
+				return false;
+
+			Piece otherPiece = (Piece)obj;
+			if (otherPiece == null)
+				return false;
+
+			if (Colour == otherPiece.Colour && Type == otherPiece.Type) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		/**
+		 * @brief Returns a string representation of the Piece object.
+		 * 
+		 * @return A string representation according to FEN notation (i.e., a black
+		 * queen is "Q" and a white bishop is "b").
+		 */
+		public override string ToString ()
+		{
+			string output = "";
+
+			switch (Type) {
+			case PieceType.Pawn:
+				output = "p";
+				break;
+			case PieceType.Knight:
+				output = "n";
+				break;
+			case PieceType.Bishop:
+				output = "b";
+				break;
+			case PieceType.Rook:
+				output = "r";
+				break;
+			case PieceType.Queen:
+				output = "q";
+				break;
+			case PieceType.King:
+				output = "k";
+				break;
+			}
+
+			if (Colour == PieceColour.Black)
+				output = output.ToUpper ();
+
+			return output;
+		}
+
+		/**
+		 * @brief Generates a hash code which C# uses to compare objects in IEnumerables.
+		 * 
+		 * @return A hash of the integer representation of the piece colour added to the
+		 * integer representation of the piece type.
+		 */
+		public override int GetHashCode ()
+		{
+			return (int)Colour + (int)Type;
+		}
+	}
+}
+
---
+++ gui/Test/BoardTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using GUI;
+
+namespace Test
+{
+	[TestFixture ()]
+	public class PieceTest
+	{
+		[Test ()]
+		public void BasicPieceTest ()
+		{
+			Piece queen = new Piece (PieceColour.White, PieceType.Queen);
+			Piece newQueen = new Piece (queen);
+			Piece blackKnight = new Piece (PieceColour.Black, PieceType.Knight);
+
+			Assert.AreEqual (queen, newQueen);
+			Assert.AreEqual (queen.GetHashCode(), newQueen.GetHashCode());
+			Assert.AreEqual ("q", queen.ToString());
+			Assert.AreEqual ("N", blackKnight.ToString ());
+		}
+	}
+}
+
