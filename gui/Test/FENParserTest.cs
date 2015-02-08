ca21f215a8ed64e86a6f0bec34ff566053d7354c
Terry Bolt <bolt.terry@gmail.com>
2015-02-08 20:34:48 +0000

    Made some changes to the tests to comply with an older version of NUnit, as the newest version is shipped separately from Mono.

--- gui/Test/FENParserTest.cs
+++ gui/Test/FENParserTest.cs
@@ -20,32 +20,50 @@ namespace Test
         [Test()]
         public void BadFENStringTest()
         {
-            Exception ex = Assert.Throws<ArgumentException> (() => new FENParser ("sdlfkjhlkJ"));
-            Assert.AreEqual (ex.Message, "Bad FEN string passed to parser.\nParameter name: fen");
+            try {
+                FENParser parser = new FENParser ("sdlfkjhlkJ");
+                Assert.Fail("Expected FENParser construction to fail.");
+            } catch(ArgumentException ex) {
+                Assert.AreEqual (ex.Message, "Bad FEN string passed to parser.\nParameter name: fen");
+            }
         }
 
         [Test()]
         public void PiecePlacementTokenTooLongTest()
         {
             FENParser parser = new FENParser ("rnbqkbnrr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-            Exception ex = Assert.Throws<ArgumentException> (() => parser.GetBoard());
-            Assert.AreEqual (ex.Message, "Bad FEN field: Piece placement.\nParameter name: fen");
+            try {
+                parser.GetBoard();
+                Assert.Fail("Expected parser.getBoard() to fail.");
+            } catch(ArgumentException ex) {
+                Assert.AreEqual (ex.Message, "Bad FEN field: Piece placement.\nParameter name: fen");
+            }
         }
 
         [Test()]
         public void BadColourToMoveTokenTest()
         {
             FENParser parser = new FENParser ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR o KQkq - 0 1");
-            Exception ex = Assert.Throws<ArgumentException> (() => parser.GetBoard());
-            Assert.AreEqual (ex.Message, "Bad FEN field: Colour to move.\nParameter name: fen");
+            try {
+                parser.GetBoard();
+                Assert.Fail("Expected parser.getBoard() to fail.");
+            } catch(ArgumentException ex) {
+                // Expected
+                Assert.AreEqual (ex.Message, "Bad FEN field: Colour to move.\nParameter name: fen");
+            }
         }
 
         [Test()]
         public void BadCastlingPossibilitiesTokenTest()
         {
             FENParser parser = new FENParser ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQQkq - 0 1");
-            Exception ex = Assert.Throws<ArgumentException> (() => parser.GetBoard());
-            Assert.AreEqual (ex.Message, "Bad FEN field: Castling possibilities.\nParameter name: fen");
+            try {
+                parser.GetBoard();
+                Assert.Fail("Expected parser.getBoard() to fail.");
+            } catch(ArgumentException ex) {
+                // Expected
+                Assert.AreEqual (ex.Message, "Bad FEN field: Castling possibilities.\nParameter name: fen");
+            }
         }
     }
 }
