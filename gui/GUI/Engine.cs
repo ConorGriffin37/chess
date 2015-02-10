8c7954959e66cbe08e3a7f91c334bf1601eb5445
Terry Bolt <bolt.terry@gmail.com>
2015-02-10 19:19:17 +0000

    Added a wrapper class called Engine for C#'s Process class. This will make it simpler to start new engine instances and communicate with them.

---
+++ gui/GUI/Engine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GUI
+{
+    public class Engine
+    {
+        Process childProcess;
+        string filename;
+
+        public Engine (string filename)
+        {
+            childProcess = new Process ();
+            this.filename = filename;
+        }
+
+        public void Start()
+        {
+            try {
+                childProcess.StartInfo.UseShellExecute = false;
+                childProcess.StartInfo.FileName = filename;
+                childProcess.StartInfo.CreateNoWindow = true;
+                childProcess.StartInfo.RedirectStandardInput = true;
+                childProcess.StartInfo.RedirectStandardOutput = true;
+                childProcess.Start();
+            } catch (Exception ex) {
+                Console.Error.WriteLine ("(EE) Error starting engine process: " + ex.Message);
+                throw new InvalidOperationException ("Process \"" + filename + "\" failed to start.");
+            }
+        }
+
+        public void Write(string message)
+        {
+            try {
+                childProcess.StandardInput.WriteLine (message);
+            } catch (Exception ex) {
+                Console.Error.WriteLine ("(EE) Error sending data to engine: " + ex.Message);
+                throw new InvalidOperationException ("Child process not running.");
+            }
+        }
+
+        public string Read()
+        {
+            string output;
+            try {
+                output = childProcess.StandardOutput.ReadLine();
+            } catch (Exception ex) {
+                Console.Error.WriteLine ("(EE) Error receiving data from engine: " + ex.Message);
+                throw new InvalidOperationException ("Child process not running.");
+            }
+            return output;
+        }
+    }
+}
+
---
+++ gui/Test/EngineTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using GUI;
+
+namespace Test
+{
+    [TestFixture ()]
+    public class EngineTest
+    {
+        [Test ()]
+        public void BadStartTest ()
+        {
+            Engine engine = new Engine("fkjflk");
+            try {
+                engine.Start();
+                Assert.Fail("Expected engine startup to fail.");
+            } catch(InvalidOperationException ex) {
+                Assert.Pass ("Engine startup failed as expected.");
+            }
+        }
+
+        [Test()]
+        public void BadWriteTest()
+        {
+            Engine engine = new Engine ("");    // The actual filename doesn't matter
+            try {
+                engine.Write("If this doesn't fail then God help me.");
+                Assert.Fail("Expected writing to engine to fail.");
+            } catch(InvalidOperationException ex) {
+                Assert.Pass ("Writing to engine failed as expected.");
+            }
+        }
+
+        [Test()]
+        public void BadReadTest()
+        {
+            Engine engine = new Engine ("");    // Filename doesn't matter
+            try {
+                engine.Read();
+                Assert.Fail("Expected reading from engine to fail.");
+            } catch(InvalidOperationException ex) {
+                Assert.Pass ("Reading from engine failed as expected.");
+            }
+        }
+    }
+}
+
