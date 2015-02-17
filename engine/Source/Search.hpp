5554c82604d4f149fd8dd108bbece762fc74c903
DarraghGriffin <darragh.griffin24@mail.dcu.ie>
2015-02-17 17:32:04 +0000

    Added doxygen documentation

--- engine/Source/Search.hpp
+++ engine/Source/Search.hpp
@@ -6,10 +6,32 @@ using namespace std;
 #include <string>
 #include "Board.hpp"
 
+/**
+ * @class Search
+ * @brief Searches the game tree for the best move
+ *
+ */
+
 class Search
 {
     public:
+        /**
+		 * @fn RootAlphaBeta
+		 * @brief Performs an Alpha-Beta search starting at the root node
+		 * @param gameBoard The game to search
+		 * @param playerColor An int representing the color of the player to make a move for. 1 for white, -1 for black
+		 * @param remainingDepth The depth to search to
+		 * @return string String representing the best move
+		 */
         string RootAlphaBeta(Board gameBoard, int playerColor, int remainingDepth);
+        /**
+		 * @fn AlphaBeta
+		 * @brief Searches the game tree to a given depth
+		 * @param board The game to search
+		 * @param playerColor An int representing the color of the player to make a move for. 1 for white, -1 for black
+		 * @param remainingDepth The depth to search to
+		 * @return int The score of the position
+		 */
         int AlphaBeta(Board board, int alpha, int beta, int remainingDepth, int playerColor);
 };
 
